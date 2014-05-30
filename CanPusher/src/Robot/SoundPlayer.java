ed8f02e964b1ca3650cf337afba8f713c647bffc
AGardea <AlexRogerG@gmail.com>
2014-05-30 16:58:58 -0600

    Added PsuedoCode Comment to SoundPlayer(No code added yet)

--- CanPusher/src/Robot/SoundPlayer.java
+++ CanPusher/src/Robot/SoundPlayer.java
@@ -1,5 +1,51 @@
 package Robot;
 
+/*
+ * SoundPlayer, implements IntervalTimerListener
+	SoundType playedSound
+	IntervalTimerManager timer
+	[override]Fire()
+		playSound()
+	getTimerManager()
+		return timer
+	SoundPlayer()
+		let playedSound be SoundType Mute
+	setSoundType(SoundType sound)
+		let playedSound be equal to sound
+	startPlaying()
+	playSound()
+		call SoundType’s playSound
+	stopPlaying()
+		let playedSound be equal to SoundType Mute
+
+	Enumeration SoundType
+		Mute
+			[override]playSound
+			[override]startUp
+				stop timer
+		SolidTone,
+			[override] playSound 	
+				play low, second-long tone
+			[override]startUp
+				set timer interval to 1 second
+				start timer
+		Beeping,
+			[override] playSound
+				play high, brief tone every half-second
+			[override] startUp
+				set timer interval to ½ second
+				start timer
+		Celebratory,
+			[override] playSound
+				play tone sequence
+			[override] startUp
+				stop timer
+
+		abstract playSound
+		abstract startUp
+
+ */
+
 public class SoundPlayer {
 
 }

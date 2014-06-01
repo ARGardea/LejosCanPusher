59110072f9298aee2c7056ac9f8f2e3b64040e7e
AGardea <AlexRogerG@gmail.com>
2014-06-01 15:29:14 -0600

    Small Changes to soundPlayer

--- CanPusher/src/Robot/SoundPlayer.java
+++ CanPusher/src/Robot/SoundPlayer.java
@@ -1,5 +1,10 @@
 package Robot;
 
+import java.util.Iterator;
+import java.util.List;
+
+import lejos.nxt.Sound;
+
 /*
  * SoundPlayer, implements IntervalTimerListener
 	SoundType playedSound
@@ -17,8 +22,30 @@ package Robot;
 		call SoundType’s playSound
 	stopPlaying()
 		let playedSound be equal to SoundType Mute
+ */
 
-	Enumeration SoundType
+public class SoundPlayer {
+	SoundType currentSound;
+	public SoundPlayer(){
+		
+	}
+	public void Fire(){
+		currentSound.playSound();
+	}
+	public void stopPlaying(){
+		
+	}
+	public void setSoundType(SoundType sound){
+		this.currentSound = sound;
+		currentSound.startUp();
+	}
+	
+}
+
+enum SoundType{
+	
+	/*
+	    Enumeration SoundType
 		Mute
 			[override]playSound
 			[override]startUp
@@ -44,12 +71,114 @@ package Robot;
 		abstract playSound
 		abstract startUp
 
- */
+	 */
+	
+	Mute{
 
-public class SoundPlayer {
+		@Override
+		public void playSound() {
+			// TODO Auto-generated method stub
+			//null behavior
+		}
 
+		@Override
+		public void startUp() {
+			// TODO Auto-generated method stub
+			//stop the timer
+		}},
+	SolidTone{
+
+		@Override
+		public void playSound() {
+			// TODO Auto-generated method stub
+			//play a second-long solid tone
+		}
+
+		@Override
+		public void startUp() {
+			// TODO Auto-generated method stub
+			//start .8 second timer
+		}},
+	Beeping{
+
+		@Override
+		public void playSound() {
+			// TODO Auto-generated method stub
+			//short, high tone
+		}
+
+		@Override
+		public void startUp() {
+			// TODO Auto-generated method stub
+			//set timer to 1/4, 1/3, or 1/2 second and start
+		}},
+	Celebratory{
+
+		@Override
+		public void playSound() {
+			// TODO Auto-generated method stub
+			 Sound.playTone(300,500);
+		     Sound.playTone(400,500);
+		     Sound.playTone(500,1000);
+		}
+
+		@Override
+		public void startUp() {
+			// TODO Auto-generated method stub
+			//stop timer
+		}};
+	
+	public abstract void playSound();
+	public abstract void startUp();
 }
 
-enum SoundType{
+class soundSequence implements Iterable{
+	List<Integer>  noteSequence;
+	long timeIntervalMillis = 500;
+	public void setSequence(List<Integer> sequence){
+		
+	}
+	
+	public void setTimeIntervalMillis(long timeIntervalMillis){
+		this.timeIntervalMillis = timeIntervalMillis;
+	}
+	
+	public long getTimeIntervalMillis(){
+		return timeIntervalMillis;
+	}
+	
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
+
+class soundSequenceIterator implements Iterator{
+	List<Integer> noteSequenceReference;
+	int currentIndex = 0;
+	
+	public void setNoteSequenceReference(List<Integer> noteSequence){
+		this.noteSequenceReference = noteSequence;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
